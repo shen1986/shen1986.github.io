51082ca79c015832e39dcf70502c9493329b7226
shenxf1986 <185693240@qq.com>
2019-03-22 14:51:22 +0800

    Site updated: 2019-03-22 14:51:20

--- js/main.js
+++ js/main.js
@@ -133,4 +133,23 @@
         }
     });
 
+    dragPanelMove("#landlord","#landlord");
+    function dragPanelMove(downDiv,moveDiv){
+        $(downDiv).mousedown(function (e) {
+                var isMove = true;
+                var div_x = e.pageX - $(moveDiv).offset().left;
+                var div_y = e.pageY - $(moveDiv).offset().top;
+                $(document).mousemove(function (e) {
+                    if (isMove) {
+                        var obj = $(moveDiv);
+                        obj.css({"left":e.pageX - div_x, "top":e.pageY - div_y});
+                    }
+                }).mouseup(
+                    function () {
+                    isMove = false;
+                });
+        });
+
+    }
+
 })(jQuery);
