7c4489080f2b83f3f2e93c285f7edad875503575
shenxf1986 <185693240@qq.com>
2019-06-04 09:30:47 +0800

    Site updated: 2019-06-04 09:30:42

--- shenxf/js/valine.js
+++ shenxf/js/valine.js
@@ -1,5 +1,25 @@
 (function(){
-    // $('#valine-thread').html();
-    // 启动提交事件
-    $(".vsubmit .vbtn").trigger('click');
+    $("#valine-thread .vcontrol .text-right").append(
+      '<button type="button" title="Cmd|Ctrl+Enter" class="vsubmit vbtn mybtn">回复</button>'
+    );
+
+    $("#valine-thread .mybtn").on('click', function() {
+        // 判断条件 昵称和邮箱必须写
+        if ($(".vnick").val().length === 0) {
+            alert('请输入昵称');
+
+            $(".vnick").focus();
+            return false;
+        }
+
+        if ($(".vmail").val().length === 0) {
+            alert("请输入邮箱");
+
+            $(".vmail").focus();
+            return false;
+        }
+
+        // 启动提交事件
+        $("#valine-thread .vbtn:not(.mybtn)").trigger("click");
+    });
 })();
